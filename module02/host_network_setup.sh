b71894877756617524740109b273ebe005ac3cb7
rnorfield <rnorfield@gmail.com>
2019-09-16 16:34:22 -0700

    Recommitting files after tagging

---
+++ module02/host_network_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+vbmg () { VBoxManage.exe "$@"; }
+export PATH=/mnt/c/Program\ Files/Oracle/VirtualBox:$PATH
+vbmg natnetwork add --netname net_4640 --network "192.168.250.0/24" --enable
+vbmg natnetwork modify --netname net_4640 --dhcp off
+vbmg natnetwork modify \
+  --netname net_4640 --port-forward-4 "ssh:tcp:[]:50022:[192.168.250.10]:22"
+vbmg natnetwork modify \
+  --netname net_4640 --port-forward-4 "http:tcp:[]:50080:[192.168.250.10]:80"
+vbmg natnetwork modify \
+  --netname net_4640 --port-forward-4 "https:tcp:[]:50443:[192.168.250.10]:443"
+vbmg natnetwork start --netname net_4640
